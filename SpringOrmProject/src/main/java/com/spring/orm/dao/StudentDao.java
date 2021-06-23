12fbf07919a5bbb6f0db978b623a64513dc4e083
bkp3 <bholakumar.7372@gmail.com>
2021-06-23 22:21:02 +0530

    CRUD operation

--- SpringOrmProject/src/main/java/com/spring/orm/dao/StudentDao.java
+++ SpringOrmProject/src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,19 +1,47 @@
 package com.spring.orm.dao;
 
+import java.util.List;
+
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.spring.orm.entites.Student;
 
 public class StudentDao {
-	
+
 	private HibernateTemplate hibernateTemplate;
-	
+
+	// save data
 	@Transactional
 	public int insert(Student student) {
-		//insert
-		Integer i = (Integer)this.hibernateTemplate.save(student);
+		// insert
+		Integer i = (Integer) this.hibernateTemplate.save(student);
 		return i;
+	}
+
+	// get the single data(object)
+	public Student getStudent(int id) {
+		Student student = this.hibernateTemplate.get(Student.class, id);
+		return student;
+	}
+
+	// get all student(all rows)
+	public List<Student> getAllStudents() {
+		List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+		return students;
+	}
+
+	// deleting the data
+	@Transactional
+	public void deleteStudent(int id) {
+		Student student = this.hibernateTemplate.get(Student.class, id);
+		this.hibernateTemplate.delete(student);
+	}
+
+	// update data
+	@Transactional
+	public void updateStudent(Student student) {
+		this.hibernateTemplate.update(student);
 	}
 
 	public HibernateTemplate getHibernateTemplate() {
@@ -23,7 +51,5 @@ public class StudentDao {
 	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
 		this.hibernateTemplate = hibernateTemplate;
 	}
-	
-	
 
 }
